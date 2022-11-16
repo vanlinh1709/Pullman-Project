9ac13af3305a232cd6e6d239281dbdc35ac4879e
Linh Tv <linhtvph18589@fpt.edu.vn>
2022-11-16 10:48:54 +0700

    add index view and controler mod user

--- core/router.php
+++ core/router.php
@@ -2,7 +2,7 @@
 //Triệu gọi đến file xử lý thông qua request
 
 $request_path = MODULESPATH . DIRECTORY_SEPARATOR . get_role() . DIRECTORY_SEPARATOR .  get_module() . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . get_controller().'Controller.php';
-//var_dump($request_path);
+var_dump($request_path);
 if (file_exists($request_path)) {
     require $request_path;
 } else {
@@ -11,7 +11,7 @@ if (file_exists($request_path)) {
 
 // get method if get default null
 $method = $_SERVER['REQUEST_METHOD'] === 'GET' ? '' : $_SERVER['REQUEST_METHOD'];
-
+//var_dump($method); $method là post hoặc get
 $action_name = get_action().ucfirst(strtolower($method)).'Action';
 //var_dump($action_name);
 call_function(array('construct', $action_name));
--- modules/admin/users/models/indexModel.php
+++ modules/admin/users/models/indexModel.php
@@ -1,11 +1,25 @@
 <?php
 
 function get_list_users() {
-    $result = db_fetch_array("SELECT * FROM `tbl_users`");
+    $result = pdo_query("SELECT * FROM `users`");
     return $result;
 }
 
 function get_user_by_id($id) {
-    $item = db_fetch_row("SELECT * FROM `tbl_users` WHERE `user_id` = {$id}");
-    return $item;
+    $sql = 'SELECT * FROM tbl_users WHERE id = ? ';
+    return pdo_query_one($sql, $id);
+
 }
+function createUser($name, $email, $age, $earn) {
+    $sql = 'INSERT INTO users(fullname, email, age, earn) VALUES(?, ?, ?, ?)';
+    pdo_execute($sql, $name, $email, $age, $earn);
+}
+function deleteUser($id) {
+    $sql = 'DELETE FROM users WHERE id = ?';
+    pdo_execute($sql, $id);
+}
+function updateUser($id, $name, $email, $age, $earn) {
+    $sql = 'UPDATE users SET fullname = ?, email = ?, age = ?, earn = ? WHERE id = ?';
+    pdo_execute($sql, $name, $email, $age, $earn, $id);
+}
+
