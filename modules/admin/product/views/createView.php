5641f4c261c3980466629a3177a0f59b82195eeb
Linh Tv <linhtvph18589@fpt.edu.vn>
2022-11-17 14:58:02 +0700

    done ulti uploadimage

--- modules/admin/product/views/createView.php
+++ modules/admin/product/views/createView.php
@@ -1,5 +1,4 @@
-
-<?php get_header('', 'Tạo mới tài khoản') ?>
+<?php get_header('', 'Tạo mới sản phẩm') ?>
 
     <!--begin::Subheader-->
     <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
@@ -7,7 +6,7 @@
             <!--begin::Info-->
             <div class="d-flex align-items-center flex-wrap mr-2">
                 <!--begin::Page Title-->
-                <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Tạo mới tài khoản</h5>
+                <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Tạo mới sản phẩm</h5>
                 <!--end::Page Title-->
             </div>
             <!--end::Info-->
@@ -22,45 +21,71 @@
             <!--begin::Card-->
             <div class="card card-custom gutter-b example example-compact">
                 <div class="card-header">
-                    <h3 class="card-title">Form thông tin tài khoản mới</h3>
+                    <h3 class="card-title">Form thông tin sản phẩm mới</h3>
                 </div>
                 <!--begin::Form-->
                 <form method="POST" action="">
                     <div class="card-body">
                         <div class="form-group">
-                            <label>Tên tài khoản</label>
-                            <input type="text" name="fullname" class="form-control" placeholder="Nhập vào tên tài khoản" />
+                            <label>Tên sản phẩm</label>
+                            <input type="text" name="name" class="form-control" placeholder="Nhập vào tên sản phẩm" />
                             <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
                         </div>
                         <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" name="email" class="form-control" placeholder="Nhập vào email" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Danh mục sản phẩm</label><br>
+                            <select class="selectpicker" name="category_id">
+                                <?php foreach($list_cates as $cate):?>
+                                <option value="<?php echo $cate['id']?>"><?php echo $cate['cate_name']?></option>
+                                <?php endforeach;?>
+                            </select>
+
                         </div>
                         <div class="form-group">
-                            <label>Mật khẩu</label>
-                            <input type="password" name="password" class="form-control" placeholder="Nhập mật khẩu" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Nhãn hiệu</label><br>
+                            <select class="selectpicker" name="brand_id">
+                                <?php foreach($list_brands as $brand):?>
+                                    <option value="<?php echo $brand['id']?>"><?php echo $brand['brand_name']?></option>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Số lượng</label>
+                            <input type="number" name="number" class="form-control" placeholder="Nhập vào số lượng sản phẩm" />
                         </div>
                         <div class="form-group">
-                            <label>Số điện thoại</label>
-                            <input type="text" name="phonenumber" class="form-control" placeholder="Nhập vào số điện thoại" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Giá sản phẩm</label>
+                            <input type="text" name="price" class="form-control" placeholder="Nhập vào giá của sản phẩm" />
                         </div>
                         <div class="form-group">
-                            <label>Địa chỉ</label>
-                            <input type="text" name="address" class="form-control" placeholder="Nhập vào địa chỉ" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Giá sản phẩm khuyến mãi</label>
+                            <input type="text" name="promo_price" class="form-control" placeholder="Nhập giá khuyến mãi" />
                         </div>
                         <div class="form-group">
-                            <label>Vai trò</label>
-                            <input type="text" name="role_id" class="form-control" placeholder="Nhập vào vai trò của tài khoản" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Ảnh đại diện của sản phẩm</label>
+                            <input type="file" name="thumbnail" class="form-control"/>
                         </div>
                         <div class="form-group">
-                            <label>Ảnh đại diện</label>
-                            <input type="file" name="name" class="form-control" placeholder="" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <div class="form-group green-border-focus">
+                                <label for="exampleFormControlTextarea5">Mô tả sản phẩm</label>
+                                <textarea class="form-control" name="des" rows="3"></textarea>
+                            </div>
+                            </textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Trạng thái sản phẩm</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="1" name="status" id="flexRadioDefault1" checked>
+                                <label class="form-check-label" for="status">
+                                   Còn hàng
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="0" name="status" id="flexRadioDefault2" >
+                                <label class="form-check-label" for=status"">
+                                    Hết hàng
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer">
--- modules/admin/product/views/updateView.php
+++ modules/admin/product/views/updateView.php
@@ -1,4 +1,4 @@
-<?php get_header('', 'Chỉnh sửa tài khoản') ?>
+<?php get_header('', 'Tạo mới sản phẩm') ?>
 
     <!--begin::Subheader-->
     <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
@@ -6,7 +6,7 @@
             <!--begin::Info-->
             <div class="d-flex align-items-center flex-wrap mr-2">
                 <!--begin::Page Title-->
-                <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Chỉnh sửa tài khoản</h5>
+                <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Chỉnh sửa sản phẩm</h5>
                 <!--end::Page Title-->
             </div>
             <!--end::Info-->
@@ -21,49 +21,83 @@
             <!--begin::Card-->
             <div class="card card-custom gutter-b example example-compact">
                 <div class="card-header">
-                    <h3 class="card-title">Form thông tin tài khoản đang chỉnh sửa</h3>
+                    <h3 class="card-title">Form thông tin sản phẩm</h3>
                 </div>
                 <!--begin::Form-->
                 <form method="POST" action="">
                     <div class="card-body">
+
                         <div class="form-group">
-                            <label>Tên tài khoản</label>
-                            <input type="text" value="<?php echo $user['fullname']?>" name="fullname" class="form-control" placeholder="Nhập vào tên tài khoản" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Tên sản phẩm</label>
+<!--                            --><?php //var_dump($product);?>
+<!--                                --><?php //var_dump($categoryOfProduct);?>
+                            <input type="text" name="name" class="form-control" value="<?php echo $product['title']?>" />
                         </div>
                         <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" value="<?php echo $user['email']?>" name="email" class="form-control" placeholder="Nhập vào email" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Danh mục sản phẩm</label><br>
+                            <select class="selectpicker" name="category_id">
+                                <option value="<?php echo $categoryOfProduct['id']?> checked"><?php echo $categoryOfProduct['cate_name']?></option>
+                                <?php foreach($list_cates as $cate):?>
+                                    <?php if($cate['id'] != $categoryOfProduct['id']):?>
+                                    <option value="<?php echo $cate['id']?>"><?php echo $cate['cate_name']?></option>
+                                    <?php endif;?>
+                                <?php endforeach;?>
+                            </select>
+
                         </div>
                         <div class="form-group">
-                            <label>Mật khẩu</label>
-                            <input type="password" value="<?php echo $user['password']?>" name="password" class="form-control" placeholder="Nhập mật khẩu" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Nhãn hiệu</label><br>
+                            <select class="selectpicker" name="brand_id">
+                                <option value="<?php echo $brandOfProduct['id']?> checked"><?php echo $brandOfProduct['brand_name']?></option>
+                                <?php foreach($list_brands as $brand):?>
+                                    <?php if($brand['id'] != $brandOfProduct['id']):?>
+                                    <option value="<?php echo $brand['id']?>"><?php echo $brand['brand_name']?></option>
+                                    <?php endif;?>
+                                <?php endforeach;?>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Số lượng</label>
+                            <input type="number" name="number" class="form-control"value="<?php echo $product['number']?>" />
                         </div>
                         <div class="form-group">
-                            <label>Số điện thoại</label>
-                            <input type="text" value="<?php echo $user['phone_number']?>" name="phonenumber" class="form-control" placeholder="Nhập vào số điện thoại" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Giá sản phẩm</label>
+                            <input type="text" name="price" class="form-control" value="<?php echo $product['price']?>" />
                         </div>
                         <div class="form-group">
-                            <label>Địa chỉ</label>
-                            <input type="text" value="<?php echo $user['address']?>" name="address" class="form-control" placeholder="Nhập vào địa chỉ" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Giá sản phẩm khuyến mãi</label>
+                            <input type="text" name="promo_price" class="form-control" value="<?php echo $product['promo_price']?>" />
                         </div>
                         <div class="form-group">
-                            <label>Vai trò</label>
-                            <input type="text" value="<?php echo $user['role_id']?>" name="role_id" class="form-control" placeholder="Nhập vào vai trò của tài khoản" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <label>Ảnh đại diện của sản phẩm</label>
+                            <input type="file" name="thumbnail" class="form-control"/>
                         </div>
                         <div class="form-group">
-                            <label>Ảnh đại diện</label>
-                            <input type="file" value="<?php echo $user['avatar']?>" name="name" class="form-control" placeholder="" />
-                            <!-- <span class="form-text text-muted">We'll never share your email with anyone else.</span> -->
+                            <div class="form-group green-border-focus">
+                                <label for="exampleFormControlTextarea5">Mô tả sản phẩm</label>
+                                <textarea class="form-control" name="des" rows="3"><?php echo $product['description']?></textarea>
+                            </div>
+                            </textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Trạng thái sản phẩm</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="<?php echo $product['status']?>" name="status" id="flexRadioDefault1" checked>
+                                <label class="form-check-label" for="status">
+                                   <?php echo $product['status'] == 1 ? 'Còn hàng' : 'Hết hàng'?>
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" value="<?php echo $product['status'] == 1 ? 0 : 1?>" name="status" id="flexRadioDefault1">
+                                <label class="form-check-label" for="status">
+                                    <?php echo $product['status'] == 1 ? 'Hết hàng' : 'Còn hàng'?>
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-primary mr-2">Cập nhập</button>
+                        <button type="submit" class="btn btn-primary mr-2">Tạo mới</button>
                         <button type="reset" class="btn btn-secondary">Làm lại</button>
                         <a href="http://localhost/Nhom_7_DA1/?role=admin&mod=users" class="btn btn-default">Quay về</a>
                     </div>
