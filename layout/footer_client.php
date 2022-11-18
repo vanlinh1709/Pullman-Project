0bdfa7478ce0563e56d8a1505db8c43e750d0247
Linh Tv <linhtvph18589@fpt.edu.vn>
2022-11-18 17:30:29 +0700

    upload css library

--- layout/footer_client.php
+++ layout/footer_client.php
@@ -1,2 +1,274 @@
-    </body>
+<!-- Scroll to top start -->
+<div class="scroll-top not-visible">
+    <i class="fa fa-angle-up"></i>
+</div>
+<!-- Scroll to Top End -->
+
+<!-- footer area start -->
+<footer class="black-bg">
+    <!-- newsletter area start -->
+    <section class="newsletter-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="newsletter-wrapper text-center">
+                        <h3 class="title text-white">JOIN OUR NEWSLETTER</h3>
+                        <form class="newsletter-inner" id="mc-form">
+                            <input type="email" class="news-field" id="mc-email" autocomplete="off" placeholder="Enter your email address">
+                            <button class="news-btn" id="mc-submit">Subscribe</button>
+                        </form>
+                        <h6 class="newsletter-subtitle">To get the latest news from us please subscribe your email.</h6>
+                        <!-- mailchimp-alerts Start -->
+                        <div class="mailchimp-alerts">
+                            <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+                            <div class="mailchimp-success"></div><!-- mailchimp-success end -->
+                            <div class="mailchimp-error"></div><!-- mailchimp-error end -->
+                        </div>
+                        <!-- mailchimp-alerts end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- newsletter area end -->
+    <div class="footer-widget-area">
+        <div class="container">
+            <div class="row mtn-30">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget-item mt-30">
+                        <h6 class="widget-title">CUSTOM LINKS</h6>
+                        <ul class="usefull-links">
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Order Status</a></li>
+                            <li><a href="#">Returns & Exchanges</a></li>
+                            <li><a href="#">Size Guide</a></li>
+                            <li><a href="#">Cart</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget-item mt-30">
+                        <h6 class="widget-title">PRODUCTS</h6>
+                        <ul class="usefull-links">
+                            <li><a href="#">Prices drop</a></li>
+                            <li><a href="#">New products</a></li>
+                            <li><a href="#">Best sales</a></li>
+                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Sitemap</a></li>
+                            <li><a href="#">Stores</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget-item mt-30">
+                        <h6 class="widget-title">OUR COMPANY</h6>
+                        <ul class="usefull-links">
+                            <li><a href="#">Delivery</a></li>
+                            <li><a href="#">Legal Notice</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Secure payment</a></li>
+                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Sitemap</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-widget-item mt-30">
+                        <h6 class="widget-title">YOUR ACCOUNT</h6>
+                        <ul class="usefull-links">
+                            <li><a href="#">Personal info</a></li>
+                            <li><a href="#">Orders</a></li>
+                            <li><a href="#">Credit slips</a></li>
+                            <li><a href="#">Addresses</a></li>
+                            <li><a href="#">Stores</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-middle-area">
+        <div class="container">
+            <div class="row mtn-30">
+                <div class="col-lg-6 col-sm-12">
+                    <div class="address-block mt-30">
+                        <div class="footer-logo">
+                            <a href="index.html">
+                                <img src="assets/img/logo/logo.png" alt="Brand Logo">
+                            </a>
+                        </div>
+                        <address class="address-info d-flex align-items-center">
+                            <i class="fa fa-map-marker"></i>
+                            <p><span>ADDRESS : </span> Your address goes here</p>
+                        </address>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <address class="address-info email mt-30">
+                        <i class="fa fa-envelope"></i>
+                        <p><span>EMAIL : </span><a href="emailto:demo@examplecom">demo@examplecom</a></p>
+                    </address>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <address class="address-info email mt-30">
+                        <i class="fa fa-phone"></i>
+                        <p><span>PHONE : </span><a href="tel:0123456789">0123456789</a></p>
+                    </address>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom-area text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="payment-method-list">
+                        <img src="assets/img/payment-2.png" alt="payment method">
+                    </div>
+                    <div class="footer-link">
+                        <a href="#">SITE MAP</a>
+                        <a href="#">SEARCH TERMS</a>
+                        <a href="#">ADVANCED SEARCH</a>
+                        <a href="#">ORDERS AND RETURNS</a>
+                        <a href="#">CONTACT US</a>
+                    </div>
+                    <p class="copyright">
+                    <p>© 2021 <b class="text-white">Pullman</b> Made with <i class="fa fa-heart text-danger"></i> by <a href="https://hasthemes.com/">HasThemes</a></p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer area end -->
+
+<!-- Quick view modal start -->
+<div class="modal" id="quick_view">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-bs-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <!-- product details inner end -->
+                <div class="product-details-inner">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <div class="product-large-slider">
+                                <div class="pro-large-img">
+                                    <img src="assets/img/product/product-details-img1.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-large-img">
+                                    <img src="assets/img/product/product-details-img2.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-large-img">
+                                    <img src="assets/img/product/product-details-img3.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-large-img">
+                                    <img src="assets/img/product/product-details-img4.jpg" alt="product-details" />
+                                </div>
+                            </div>
+                            <div class="pro-nav slick-row-10 slick-arrow-style">
+                                <div class="pro-nav-thumb">
+                                    <img src="assets/img/product/product-details-img1.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-nav-thumb">
+                                    <img src="assets/img/product/product-details-img2.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-nav-thumb">
+                                    <img src="assets/img/product/product-details-img3.jpg" alt="product-details" />
+                                </div>
+                                <div class="pro-nav-thumb">
+                                    <img src="assets/img/product/product-details-img4.jpg" alt="product-details" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="product-details-des">
+                                <h3 class="product-name">Premium Mens Sports Lather Keds</h3>
+                                <div class="ratings d-flex">
+                                    <span><i class="fa fa-star"></i></span>
+                                    <span><i class="fa fa-star"></i></span>
+                                    <span><i class="fa fa-star"></i></span>
+                                    <span><i class="fa fa-star"></i></span>
+                                    <span><i class="fa fa-star"></i></span>
+                                    <div class="pro-review">
+                                        <span>1 Reviews</span>
+                                    </div>
+                                </div>
+                                <div class="price-box">
+                                    <span class="price-old"><del>$90.00</del></span>
+                                    <span class="price-regular">$70.00</span>
+                                </div>
+                                <h5 class="offer-text"><strong>Hurry up</strong>! offer ends in:</h5>
+                                <div class="product-countdown" data-countdown="2021/09/20"></div>
+                                <p class="pro-desc">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+                                <div class="quantity-cart-box d-flex align-items-center">
+                                    <h6 class="option-title">qty:</h6>
+                                    <div class="quantity">
+                                        <div class="pro-qty"><input type="text" value="1"></div>
+                                    </div>
+                                    <div class="action_link">
+                                        <a class="btn btn-cart2" href="#">Add To Cart</a>
+                                    </div>
+                                </div>
+                                <div class="useful-links">
+                                    <a href="#" data-bs-toggle="tooltip" title="Compare"><i
+                                                class="fa fa-refresh"></i>compare</a>
+                                    <a href="#" data-bs-toggle="tooltip" title="Wishlist"><i
+                                                class="fa fa-heart-o"></i>wishlist</a>
+                                </div>
+                                <div class="like-icon">
+                                    <a class="facebook" href="#"><i class="fa fa-facebook"></i>like</a>
+                                    <a class="twitter" href="#"><i class="fa fa-twitter"></i>tweet</a>
+                                    <a class="pinterest" href="#"><i class="fa fa-pinterest"></i>save</a>
+                                    <a class="google" href="#"><i class="fa fa-google-plus"></i>share</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- product details inner end -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Quick view modal end -->
+
+<!-- JS
+============================================ -->
+
+<!-- Modernizer JS -->
+<script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+<!-- jQuery JS -->
+<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+<!-- slick Slider JS -->
+<script src="assets/js/plugins/slick.min.js"></script>
+<!-- Countdown JS -->
+<script src="assets/js/plugins/countdown.min.js"></script>
+<!-- Nice Select JS -->
+<script src="assets/js/plugins/nice-select.min.js"></script>
+<!-- jquery UI JS -->
+<script src="assets/js/plugins/jqueryui.min.js"></script>
+<!-- Image zoom JS -->
+<script src="assets/js/plugins/image-zoom.min.js"></script>
+<!-- image loaded js -->
+<script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+<!-- masonry  -->
+<script src="assets/js/plugins/masonry.pkgd.min.js"></script>
+<!-- mailchimp active js -->
+<script src="assets/js/plugins/ajaxchimp.js"></script>
+<!-- contact form dynamic js -->
+<script src="assets/js/plugins/ajax-mail.js"></script>
+<!-- google map api -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfmCVTjRI007pC1Yk2o2d_EhgkjTsFVN8"></script>
+<!-- google map active js -->
+<script src="assets/js/plugins/google-map.js"></script>
+<!-- Main JS -->
+<script src="assets/js/main.js"></script>
+</body>
 </html>
